d57847b4e0d83a118b9ae1ba55929e9b5e5bbb8c
peterorlowsky <moritz@netcandy.de>
2018-04-25 16:25:09 +0200

    add localized headlines to each selection group

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const locale = 'de'; // 'set to 'en' for english localisation
 let selectedId = '';
+const headlines = {
+    de: {
+        PET: ['Typ', 'Farbe', 'Beschichtung', 'Auswahl 4', 'Auswahl 5'],
+        OPA: ['Typ', 'Auswahl2', 'Beschichtung', 'Auswahl 4', 'Auswahl 5'],
+        BOPP: ['Typ', 'Farbe', 'Beschichtung', 'Auswahl 4', 'Auswahl 5'],
+        Aluminium: ['Typ', 'Farbe', 'Beschichtung', 'Auswahl 4', 'Auswahl 5'],
+        Reserviert: ['Typ']
+    },
+    en: {
+        PET: ['type', 'color', 'surface', 'selection 4', 'selection 5'],
+        OPA: ['type', 'color', 'surface', 'selection 4', 'selection 5'],
+        BOPP: ['type', 'color', 'surface', 'selection 4', 'selection 5'],
+        Aluminium: ['type', 'color', 'surface', 'selection 4', 'selection 5'],
+        Reserved: ['type']
+    }
+};
 // run into CORS while serving .json local on Chrome - run in firefox for developent
 const jsonRequest = new Request.JSON({
     url: 'navigation.json',
     headers: {'Access-Control-Allow-Headers': ' X-Requested-With'},
     onSuccess: function(navigation){
-        buildMultiSelection(navigation, '.nav.site-nav', '.flyout').inject('ms-wrapper')
+        buildMultiSelection(navigation, '.nav.site-nav', '.flyout', 0, 'PET').inject('ms-wrapper')
     }}).send();
 
-function buildMultiSelection(navigation, ulClasses, liClasses) {
+function buildMultiSelection(navigation, ulClasses, liClasses,level, type) {
     let multiSelection = new Element('ul' + ulClasses);
+    let headline = new Element('h3');
+    headline.inject(multiSelection);
     navigation.forEach(function(selectable) {
+        if (level === 0) type = locale === 'de'? selectable.titleDe: selectable.titleEn;
         let listElement = new Element('li' + liClasses).grab(
             new Element('a',{
                 text: locale === 'de'? selectable.titleDe: selectable.titleEn,
@@ -26,10 +45,13 @@ function buildMultiSelection(navigation, ulClasses, liClasses) {
             })
         );
         if(selectable.children != null){
-            listElement.grab(buildMultiSelection(selectable.children, '.flyout-content.nav.stacked', '.flyout-alt'));
+            listElement.grab(buildMultiSelection(selectable.children, '.flyout-content.nav.stacked', '.flyout-alt', level+1, type));
         }
         multiSelection.grab(listElement);
     });
+    console.log(level, type);
+    headline.set('text', headlines[locale][type][level]);
+    // headline.inject(multiSelection)
     return multiSelection;
 }
 function enableSearch(executable){
